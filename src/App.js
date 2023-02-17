a818a4fbacbff6757cff506d2ca4f3f986f3859b
Becca <rkinnee91@hotmail.com>
2023-02-17 12:49:32 -0800

    header finished

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.scss';
+
+//Components
+import Header from './Components/Header/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
+      <BrowserRouter>
+        <Routes>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,13 @@
 import './Header.scss';
 
-
 function Header() {
     return (
         <>
+        <div className="header">
+            <h1 className="header__title">Arch Magazine</h1>
+            <h6 className="header__subTitle">Architecture From Around the World</h6>
+        </div>
+        
         </>
     )
 }
