a678a091a8908a75d7933e92cc9e92735fe58d5b
Becca <rkinnee91@hotmail.com>
2023-02-17 13:57:52 -0800

    set up link routes

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import './App.scss';
 
 //Components
 import Header from './Components/Header/Header';
+import Navbar from './Components/NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Navbar/>
       <BrowserRouter>
         <Routes>
+          {/* <Route path='/' element={<Home/>}/> */}
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,17 @@
+import { Link } from 'react-router-dom';
+import './NavBar.scss';
+
+function Navbar() {
+    return (
+        <>
+        <div className='navbar'>
+            {/* <Link to='/' className='navbar__link'>Home</Link> */}
+            {/* <Link to='/about' className='navbar__link'>About</Link>
+            <Link to='/contact' className='navbar__link'>Contact</Link>
+ */}
+        </div>
+        </>
+    )
+}
+
+export default Navbar;
