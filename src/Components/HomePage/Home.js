8bc17bcb04b27bb04578ec7ac957c9fff2ca9b65
Becca <rkinnee91@hotmail.com>
2023-02-23 15:04:44 -0800

    start on homepage

--- src/Components/HomePage/Home.js
+++ src/Components/HomePage/Home.js
@@ -1,9 +1,14 @@
 import './Home.scss';
+import Paragraph from '../Paragraph/Paragraph';
 
 function Home() {
     return (
-        <>
-        </>
+        <div className="home">
+            <h3 className="home__title">Vestibulum Dictum Nulla</h3>
+            <Paragraph/>
+            <Paragraph/>
+            <Paragraph/>
+        </div>
     )
 }
 
---
+++ src/Components/Paragraph/Paragraph.js
@@ -0,0 +1,20 @@
+import './Paragraph.scss'
+
+function Paragraph() {
+    return (
+        <p className="paragraph">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            Etiam erat dui, luctus sed interdum suscipit, mattis id nulla. 
+            Nam et lacus vestibulum, ultrices metus vitae, eleifend metus. 
+            Nam ornare semper ipsum, vitae tincidunt est congue ac. Phasellus tellus sapien, 
+            dapibus in convallis vitae, facilisis sed nunc. Vestibulum dictum nulla eget 
+            massa luctus elementum. Proin leo massa, rutrum non cursus placerat, gravida et purus. 
+            Aenean eu auctor neque. Vivamus urna felis, venenatis sit amet sagittis ac, accumsan in diam. 
+            Pellentesque ac dolor est. Nam sit amet interdum elit, at iaculis nulla. Quisque hendrerit, nisl nec ultricies ornare, 
+            enim turpis auctor purus, suscipit rutrum augue nisi non lorem.
+        </p>
+
+    )
+}
+
+export default Paragraph;
