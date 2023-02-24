2e1fe6298f0250cc58db62e0c240891cec8cfb08
Becca <rkinnee91@hotmail.com>
2023-02-24 12:02:30 -0800

    correct size of images @ all 3 breakpoints

--- src/Components/HomePage/Home.js
+++ src/Components/HomePage/Home.js
@@ -1,13 +1,16 @@
 import './Home.scss';
 import Paragraph from '../Paragraph/Paragraph';
-import girlAndBuilding from '../../Assets/Images/girlAndBuilding.jpg'
+import GirlAndBuilding from '../../Assets/Images/girlAndBuilding.jpg'
+import OutdoorBuilding from '../../Assets/Images/outdoorBuilding.jpg'
+
 function Home() {
     return (
         <div className="home">
             <h3 className="home__title">Vestibulum Dictum Nulla</h3>
             <Paragraph/>
-            <img src={girlAndBuilding} alt="girl standing in front of building" className="home__image"/>
+            <img src={GirlAndBuilding} alt="girl standing in front of building" className="home__image"/>
             <Paragraph/>
+            <img src={OutdoorBuilding} alt="girl standing in front of building" className="home__image--two"/>
             <Paragraph/>
         </div>
     )
