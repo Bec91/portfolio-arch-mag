e8c9c206c235a1ba5af644745a5c39e41313c739
Becca <rkinnee91@hotmail.com>
2023-02-24 11:26:29 -0800

    add photo and get correct height for all three breakpoints

--- src/Components/HomePage/Home.js
+++ src/Components/HomePage/Home.js
@@ -1,11 +1,12 @@
 import './Home.scss';
 import Paragraph from '../Paragraph/Paragraph';
-
+import girlAndBuilding from '../../Assets/Images/girlAndBuilding.jpg'
 function Home() {
     return (
         <div className="home">
             <h3 className="home__title">Vestibulum Dictum Nulla</h3>
             <Paragraph/>
+            <img src={girlAndBuilding} alt="girl standing in front of building" className="home__image"/>
             <Paragraph/>
             <Paragraph/>
         </div>
