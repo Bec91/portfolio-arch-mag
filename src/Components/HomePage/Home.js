4558e1cf670b7217667b53e6c8f7b382424a21f4
Becca <rkinnee91@hotmail.com>
2023-02-25 14:36:28 -0800

    finish homepage on all 3 viewports

--- src/Components/HomePage/Home.js
+++ src/Components/HomePage/Home.js
@@ -1,19 +1,36 @@
 import './Home.scss';
 import Paragraph from '../Paragraph/Paragraph';
 import GirlAndBuilding from '../../Assets/Images/girlAndBuilding.jpg'
-import OutdoorBuilding from '../../Assets/Images/outdoorBuilding.jpg'
+import Hallway from '../../Assets/Images/hallway.jpg'
 
 function Home() {
     return (
         <div className="home">
-            <h3 className="home__title">Vestibulum Dictum Nulla</h3>
-            <Paragraph/>
-            <div className="home__imageContainer">
-                <img src={GirlAndBuilding} alt="girl standing in front of building" className="home__image"/>
+            <div className="home__titleContainer">
+                <h3 className="home__title">Vestibulum Dictum Nulla</h3>
             </div>
-            <Paragraph/>
-            <img src={OutdoorBuilding} alt="girl standing in front of building" className="home__image--two"/>
-            <Paragraph/>
+            <div className="home__contentContainer">
+                <div className="home__paraContainer">
+                    <Paragraph/>
+                    <Paragraph/>
+                </div>
+                <div className="home__imageContainer">
+                    <img src={GirlAndBuilding} alt="girl standing in front of building" className="home__image"/>
+                </div>
+            </div>
+            <div className="home__titleContainer">
+                <h3 className="home__title">Vestibulum Dictum Nulla</h3>
+            </div>
+            <div className="home__contentContainer">
+                <div className="home__paraContainer">
+                    <Paragraph/>
+                    <Paragraph/>
+                </div>
+                <div className="home__imageContainer">
+                    <img src={Hallway} alt="girl standing in front of building" className="home__image home__image--two"/>
+                </div>
+            </div>
+
         </div>
     )
 }
