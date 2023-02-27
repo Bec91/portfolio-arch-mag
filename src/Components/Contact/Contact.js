9c7b5ccb540646a08358d66f43c478fde13ac890
Becca <rkinnee91@hotmail.com>
2023-02-27 14:47:07 -0800

    finish contact form. Correct homepage screen size(ADD monitor breakpoint). Correct About page padding.

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -2,11 +2,11 @@ import './Contact.scss'
 import React from "react"
 
 function Contact() {
-    const [formStatus, setFormStatus] = React.useState('Send');
+    const [formStatus, setFormStatus] = React.useState('SEND');
 
     const onSubmit = (e) => {
         e.preventDefault()
-        setFormStatus('Submitting...')
+        setFormStatus('Submitted')
 
         const { name, email, message } = e.target.elements
         let conFom = {
@@ -18,36 +18,49 @@ function Contact() {
     }
 
     return(
-        <form
-            onSubmit={onSubmit}
-            method="POST"
-            target='_blank'
-        >
-            <div>
-                <label htmlFor='name'>Name</label>
-                <input
-                    type="text"
-                    id='name'
-                    required
-                />
-            </div>
-            <div>
-            <label htmlFor='email'>Email</label>
-                <input
-                    type="email"
-                    id='email'
-                    required
-                />
-            </div>
-            <div>
-            <label htmlFor='message'>Message</label>
-                <textarea 
-                    id='message'
-                    required
-                />
-            </div>
-            <button type='submit'>{formStatus}</button>
-        </form>
+        <div className='form'>
+            <h3 className='form__formTitle'>Contact Us to be featured</h3>
+            <form
+                onSubmit={onSubmit}
+                method="POST"
+                target='_blank'
+            >
+                <div className='form__inputContainer'>
+                    <label className='form__label' htmlFor='name'>Name</label>
+                    <input
+                        className='form__formField'
+                        type="text"
+                        id='name'
+                        required
+                    />
+                </div>
+                <div className='form__inputContainer'>
+                <label className='form__label' htmlFor='email'>Email</label>
+                    <input
+                        className='form__formField'
+                        type="email"
+                        id='email'
+                        required
+                    />
+                </div>
+                <div className='form__inputContainer'>
+                <label className='form__label' htmlFor='message'>Message</label>
+                    <textarea
+                        className='form__formField'
+                        id='message'
+                        required
+                    />
+                </div>
+                <div className='form__buttonContainer'>
+                    <button
+                        type='submit'
+                        className='form__button'
+                    >
+                        {formStatus}
+                    </button>
+                </div>
+            </form>
+        </div>
     )
 }
 
